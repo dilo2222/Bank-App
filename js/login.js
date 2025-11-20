e9b8dfb749f552cff8a4b0a50cc53254adac91ba
Asilbek Sadiyev <201261859+dilo2222@users.noreply.github.com>
2025-11-20 16:02:19 +0500

    registration page is ready

--- js/login.js
+++ js/login.js
@@ -7,20 +7,28 @@ export function login(container) {
      <input class="login-passsword" placeholder="password" name="passwordElement"></input>
       <button type="submit">Login</button>
       <span class="login-reset">forgot login or password?</span>
+      <button class="linkToRegister">New User?</button>
     </form>`;
 
   container.append(appWrapper);
 }
 
+
+
 export function register(container) {
   const appWrapper = document.createElement("div");
 
   appWrapper.innerHTML = `
-    <form class="login-form" type="submit">
+    <form class="register-form" type="submit">
+    <h2 class="register-titile">Register</h2>
     <input class="register-name" placeholder="login" name="registerElementName"></input>
     <input class="register-surname" placeholder="login" name="registerElementSurname"></input>
      <input class="login-passsword" placeholder="password" name="registerElement"></input>
-      <button type="submit">Register</button>
+     <div class="register-inner">
+     <button type="submit">Register</button>
+      <div class="linkToLogin"><span>already registered?</span> <button class="btnToLogin">Login</button> </div>
+      </div>
+      
     </form>`;
 
   container.append(appWrapper);
