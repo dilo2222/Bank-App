80881839b6f5bf213a002fd1a8bd5315660ee227
Asilbek Sadiyev <201261859+dilo2222@users.noreply.github.com>
2025-11-23 00:05:14 +0500

    added some styles

--- js/profilePage.js
+++ js/profilePage.js
@@ -4,19 +4,41 @@ export function profile(container) {
     profileEl.classList.add("profile-container")
     profileEl.innerHTML = `
     <div>
-    <a>
+    <ul>
+    <li><a>
     <svg width="20" height="15"><use xlink:href="img/sprite.svg#profile-icon" ></use></svg>
     <span>Profile Settings</span>
-    </a>
-    <a>
+    </a></li>
+    <li><a>
     <svg width="20" height="20"><use xlink:href="img/sprite.svg#list-icon" ></use></svg>
     <span>Transaction History</span>
-    </a>
+    </a></li>
+    <li><a>
+    <svg width="20" height="15"><use xlink:href="img/sprite.svg#currency-icon" ></use></svg>
+    <span>Currency Exchange</span>
+    </a></li>
+    </ul>
+    
     </div>
 
-    <div></div>
+    <div>Balance</div>
 
-    <div></div>`
+    <div>
+    <ul>
+    <li><a>
+    <svg width="20" height="15"><use xlink:href="img/sprite.svg#profile-icon" ></use></svg>
+    <span>Profile Settings</span>
+    </a></li>
+    <li><a>
+    <svg width="20" height="20"><use xlink:href="img/sprite.svg#list-icon" ></use></svg>
+    <span>Transaction History</span>
+    </a></li>
+    <li><a>
+    <svg width="20" height="15"><use xlink:href="img/sprite.svg#currency-icon" ></use></svg>
+    <span>Currency Exchange</span>
+    </a></li>
+    </ul>
+    </div>`
 
     container.append(profileEl)
 }
